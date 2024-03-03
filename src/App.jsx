16b13adf72efdaafdcc474b9f1c7fa8633a16533
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-03-04 03:08:06 +0530

    use_memo_cont..

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
+  function increaseCount(){
+    setCount(prev => prev + 1);
+  }
 
+  function doSum(e){
+    let val = e.target.value;
+    let sum = 0;
+    for (let i = 0; i <= val; i++){
+      sum += i
+    }
+    setTotal(sum)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input style = {{
+        padding : '10px'
+        }} type="text" placeholder='value' onChange = {doSum}/><br /><br />
+        <p>{`Sum is ${total}`}</p>
+      <button onClick = {increaseCount}>{`Count ${count}`}</button>
+    </div>
   )
 }
 
